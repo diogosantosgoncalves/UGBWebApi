4bc11385cbaf67437d04cf0db09a24ee74796c8a
Diogo <Diogo@DESKTOP-K6KF994>
2022-02-24 00:02:19 -0300

    criando app tcc ugb, fazendo menu

---
+++ APPTCCUGB/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using APPTCCUGB.ViewModel;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace APPTCCUGB
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var menuRegister = new List<SubItem>();
+            menuRegister.Add(new SubItem("Cadastro", new UserControlCustomers()));
+            menuRegister.Add(new SubItem("Consulta", new UserControlProviders()));
+            menuRegister.Add(new SubItem("Employees"));
+            menuRegister.Add(new SubItem("Products"));
+            var item6 = new ItemMenu("Usuários", menuRegister, PackIconKind.Register);
+
+            var menuSchedule = new List<SubItem>();
+            menuSchedule.Add(new SubItem("Cadastro"));
+            menuSchedule.Add(new SubItem("Consulta"));
+            var item1 = new ItemMenu("Produtos", menuSchedule, PackIconKind.Factory);
+
+            var menuReports = new List<SubItem>();
+            menuReports.Add(new SubItem("Customers"));
+            menuReports.Add(new SubItem("Providers"));
+            menuReports.Add(new SubItem("Products"));
+            menuReports.Add(new SubItem("Stock"));
+            menuReports.Add(new SubItem("Sales"));
+            var item2 = new ItemMenu("Relatórios", menuReports, PackIconKind.FileReport);
+
+            var menuExpenses = new List<SubItem>();
+            menuExpenses.Add(new SubItem("Cadastro"));
+            menuExpenses.Add(new SubItem("Consulta"));
+            var item3 = new ItemMenu("Turno", menuExpenses, PackIconKind.Schedule);
+
+            var menuFinancial = new List<SubItem>();
+            menuFinancial.Add(new SubItem("Cadastro"));
+            var item4 = new ItemMenu("Setor", menuFinancial, PackIconKind.AccountGroup);
+
+            Menu.Children.Add(new UserControlMenuItem(item6, this));
+            Menu.Children.Add(new UserControlMenuItem(item1, this));
+            Menu.Children.Add(new UserControlMenuItem(item2, this));
+            Menu.Children.Add(new UserControlMenuItem(item3, this));
+            Menu.Children.Add(new UserControlMenuItem(item4, this));
+        }
+
+        internal void SwitchScreen(object sender)
+        {
+            var screen = ((UserControl)sender);
+
+            if (screen != null)
+            {
+                StackPanelMain.Children.Clear();
+                StackPanelMain.Children.Add(screen);
+            }
+        }
+    }
+}
