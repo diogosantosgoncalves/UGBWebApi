9fe8b883ff641c5a74c840c0e5b4c8511e98ef88
Diogo <Diogo@DESKTOP-K6KF994>
2022-06-29 00:06:05 -0300

    removido campo na tabela setores

---
+++ UGBWebApi/Migrations/20220629025547_RemoverCampoSetor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UGBWebApi.Migrations
+{
+    public partial class RemoverCampoSetor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HorasProducao",
+                table: "Setores");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "HorasProducao",
+                table: "Setores",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
